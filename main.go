22a4af1133b8a8274c089a6e5a3f48719adc3ed6
NyaShinn1204 <134133778+NyaShinn1204@users.noreply.github.com>
2024-03-13 17:36:13 +0900

    Update Main Program To V2
    Code optimization
    Fix Get Method

--- main.go
+++ main.go
@@ -6,14 +6,13 @@ import (
 	"log"
 	"net/http"
 	"os"
-	"sync"
-	"time"
 
 	"github.com/gorilla/websocket"
 )
 
-var users []map[string]interface{}
-var usersMutex sync.Mutex
+var token string
+var serverID string
+var channelID string
 
 func main() {
 	args := os.Args[1:]
@@ -32,34 +31,35 @@ func main() {
 	fmt.Printf("Token: %s\n", token)
 	response := checktoken(token)
 	if response == 200 {
-		log.Printf("[VALID]   %v [%v]\n", token, response)
+		fmt.Printf("[VALID]   %v [%v]\n", token, response)
 	} else {
-		log.Printf("[INVALID] %v [%v]", token, response)
+		fmt.Printf("[INVALID] %v [%v]", token, response)
 		os.Exit(1)
 	}
 	fmt.Printf("Scraping in %s with %s\n", serverID, token)
-	members := scrape(token, serverID, channelID)
+	members := getMembers(token, serverID, channelID)
 	fmt.Printf("Total Scrapped: %d\n", len(members))
 	userIDs := make([]string, len(members))
 	for i, member := range members {
-		userIDs[i] = member["id"].(string)
+		userIDs[i] = member
 	}
 
 	fmt.Printf("Scrapped Id: %v\n", userIDs)
+
 }
 
 func checktoken(token string) int {
 	url := "https://discord.com/api/v9/users/@me/affinities/guilds"
 	req, err := http.NewRequest("GET", url, nil)
 	if err != nil {
-		log.Printf("%v", err)
+		fmt.Printf("%v", err)
 		return -1
 	}
 	req.Header.Set("authorization", token)
 	httpClient := &http.Client{}
 	resp, err := httpClient.Do(commonheaders(req))
 	if err != nil {
-		log.Printf("%v", err)
+		fmt.Printf("%v", err)
 		return -1
 	}
 	return resp.StatusCode
@@ -81,302 +81,97 @@ func commonheaders(req *http.Request) *http.Request {
 	return req
 }
 
-func rangeCorrector(ranges [][]int) [][]int {
-	if !containsRange(ranges, []int{0, 99}) {
-		ranges = append([][]int{{0, 99}}, ranges...)
+func getMembers(token, server, channel string) []string {
+	conn, _, err := websocket.DefaultDialer.Dial("wss://gateway.discord.gg/?v=10&encoding=json", nil)
+	if err != nil {
+		log.Fatal("dial:", err)
 	}
-	return ranges
-}
+	defer conn.Close()
 
-func getRanges(index, multiplier, memberCount int) [][]int {
-	initialNum := index * multiplier
-	rangesList := [][]int{{initialNum, initialNum + 99}}
-	if memberCount > initialNum+99 {
-		rangesList = append(rangesList, []int{initialNum + 100, initialNum + 199})
-	}
-	return rangeCorrector(rangesList)
-}
-
-func parseGuildMemberListUpdate(response map[string]interface{}) map[string]interface{} {
-	memberdata := map[string]interface{}{
-		"online_count":  response["d"].(map[string]interface{})["online_count"],
-		"member_count":  response["d"].(map[string]interface{})["member_count"],
-		"id":            response["d"].(map[string]interface{})["id"],
-		"guild_id":      response["d"].(map[string]interface{})["guild_id"],
-		"hoisted_roles": response["d"].(map[string]interface{})["groups"],
-		"types":         []interface{}{},
-		"locations":     []interface{}{},
-		"updates":       []interface{}{},
-	}
-
-	for _, chunk := range response["d"].(map[string]interface{})["ops"].([]interface{}) {
-		memberdata["types"] = append(memberdata["types"].([]interface{}), chunk.(map[string]interface{})["op"])
-		if chunk.(map[string]interface{})["op"] == "SYNC" || chunk.(map[string]interface{})["op"] == "INVALIDATE" {
-			memberdata["locations"] = append(memberdata["locations"].([]interface{}), chunk.(map[string]interface{})["range"])
-			if chunk.(map[string]interface{})["op"] == "SYNC" {
-				memberdata["updates"] = append(memberdata["updates"].([]interface{}), chunk.(map[string]interface{})["items"])
-			} else {
-				memberdata["updates"] = append(memberdata["updates"].([]interface{}), []interface{}{})
-			}
-		} else if chunk.(map[string]interface{})["op"] == "INSERT" || chunk.(map[string]interface{})["op"] == "UPDATE" || chunk.(map[string]interface{})["op"] == "DELETE" {
-			memberdata["locations"] = append(memberdata["locations"].([]interface{}), chunk.(map[string]interface{})["index"])
-			if chunk.(map[string]interface{})["op"] == "DELETE" {
-				memberdata["updates"] = append(memberdata["updates"].([]interface{}), []interface{}{})
-			} else {
-				memberdata["updates"] = append(memberdata["updates"].([]interface{}), chunk.(map[string]interface{})["item"])
-			}
-		}
-	}
-
-	return memberdata
-}
-
-type DiscordSocket struct {
-	Token         string
-	GuildID       string
-	ChannelID     string
-	SocketHeaders map[string]string
-	Conn          *websocket.Conn
-	EndScraping   bool
-	Guilds        map[string]map[string]interface{}
-	Members       map[string]interface{}
-	Ranges        [][]int
-	LastRange     int
-	PacketsRecv   int
-	Mutex         sync.Mutex
-}
-
-func (ds *DiscordSocket) run() {
-	ds.runForever()
-}
-
-func (ds *DiscordSocket) scrapeUsers() {
-	log.Println("scrapeUsers called")
-	if !ds.EndScraping {
-		log.Println("Sending scrape request...")
-		rangesJSON, err := json.Marshal(ds.Ranges)
-		if err != nil {
-			log.Println("Error marshalling Ranges:", err)
-			return
-		}
-		fmt.Println(rangesJSON)
-
-		requestJSON1 := `{"op":14,"d":{"guild_id":"` + ds.GuildID + `","typing":true,"activities":true,"threads":true,"channels":{"` + ds.ChannelID + `":` + string(rangesJSON) + `}}}`
-		fmt.Println(requestJSON1)
-		ds.send(requestJSON1)
-		requestJSON2 := `{"op":14,"d":{"guild_id":"` + ds.GuildID + `","typing":true,"activities":true,"threads":true,"channels":{"` + ds.ChannelID + `":[[0, 99], [100, 199]]}}}`
-		fmt.Println(requestJSON2)
-		ds.send(requestJSON2)
-	}
-}
-
-func (ds *DiscordSocket) sockOpen() {
-	ds.send(fmt.Sprintf(`{"op":2,"d":{"token":"%s","capabilities":125,"properties":{"os":"Windows","browser":"Firefox","device":"","system_locale":"it-IT","browser_user_agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:94.0) Gecko/20100101 Firefox/94.0","browser_version":"94.0","os_version":"10","referrer":"","referring_domain":"","referrer_current":"","referring_domain_current":"","release_channel":"stable","client_build_number":103981,"client_event_source":null},"presence":{"status":"online","since":0,"activities":[],"afk":false},"compress":false,"client_state":{"guild_hashes":{},"highest_last_message_id":"0","read_state_version":0,"user_guild_settings_version":-1,"user_settings_version":-1}}}`, ds.Token))
-}
-
-func (ds *DiscordSocket) heartbeatThread(interval time.Duration) {
+	users := []string{}
 	for {
-		ds.send(fmt.Sprintf(`{"op":1,"d":%d}`, ds.PacketsRecv))
-		time.Sleep(interval)
-	}
-}
+		_, message, err := conn.ReadMessage()
+		if err != nil {
+			log.Println("read:", err)
+			return nil
+		}
 
-func (ds *DiscordSocket) sockMessage(message []byte) {
-	//log.Println("Received message:", string(message))
+		var response map[string]interface{}
+		if err := json.Unmarshal(message, &response); err != nil {
+			log.Println("unmarshal:", err)
+			return nil
+		}
 
-	var decoded map[string]interface{}
-	err := json.Unmarshal(message, &decoded)
-	if err != nil || decoded == nil {
-		log.Println("Error decoding message:", err)
-		return
-	}
-
-	ds.Mutex.Lock()
-	defer ds.Mutex.Unlock()
-
-	if decoded["op"].(float64) != 11 {
-		ds.PacketsRecv++
-	}
-
-	switch decoded["op"].(float64) {
-	case 10:
-		log.Println("Received OP 10 (Hello)")
-		go ds.heartbeatThread(time.Duration(decoded["d"].(map[string]interface{})["heartbeat_interval"].(float64)) * time.Millisecond)
-	case 0:
-		switch decoded["t"].(string) {
-		case "READY":
-			log.Println("Received READY event")
-			for _, guild := range decoded["d"].(map[string]interface{})["guilds"].([]interface{}) {
-				guildID := guild.(map[string]interface{})["id"].(string)
-				ds.Guilds[guildID] = map[string]interface{}{
-					"member_count": guild.(map[string]interface{})["member_count"].(float64),
-				}
-				log.Println("Added Guild ID to ds.Guilds:", guildID)
-				guild, ok := ds.Guilds[guildID]
-				if !ok || guild == nil {
-					log.Printf("Error: Guild not found for ID %s or it is nil\n", guildID)
-					os.Exit(1)
-					//return
-				}
+		if response["t"] == nil {
+			sendData := map[string]interface{}{
+				"op": 2,
+				"d": map[string]interface{}{
+					"token":        token,
+					"capabilities": 16381,
+					"properties": map[string]interface{}{
+						"os":                       "Android",
+						"browser":                  "Discord Android",
+						"device":                   "Android",
+						"system_locale":            "ja-JP",
+						"browser_user_agent":       "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36",
+						"browser_version":          "122.0.0.0",
+						"os_version":               "",
+						"referrer":                 "",
+						"referring_domain":         "",
+						"referrer_current":         "",
+						"referring_domain_current": "",
+						"release_channel":          "stable",
+						"client_build_number":      263582,
+						"client_event_source":      nil,
+					},
+					"presence": map[string]interface{}{
+						"status":     "invisible",
+						"since":      0,
+						"activities": []interface{}{},
+						"afk":        false,
+					},
+					"compress": false,
+					"client_state": map[string]interface{}{
+						"guild_versions":              map[string]interface{}{},
+						"highest_last_message_id":     "0",
+						"read_state_version":          0,
+						"user_guild_settings_version": -1,
+						"private_channels_version":    "0",
+						"api_code_version":            0,
+					},
+				},
 			}
-		case "READY_SUPPLEMENTAL":
-			if decoded["t"].(string) == "READY_SUPPLEMENTAL" {
-				log.Println("Received READY_SUPPLEMENTAL event") // この行を追加
-				//ds.Mutex.Lock()
-				//defer ds.Mutex.Unlock()
-				guildID := ds.GuildID
-				if guildID == "" {
-					log.Println("Error: GuildID is empty")
-					return
-				}
-
-				guild, ok := ds.Guilds[guildID]
-				if !ok || guild == nil {
-					log.Printf("Error: Guild not found for ID %s or it is nil\n", guildID)
-					//os.Exit(1)
-					//return
-				}
-
-				memberCount, ok := guild["member_count"].(float64)
-				if !ok {
-					log.Println("Error: Unable to get member count from Guild")
-					//return
-				}
-
-				ds.Ranges = getRanges(0, 100, int(memberCount))
-				ds.scrapeUsers()
+			if err := conn.WriteJSON(sendData); err != nil {
+				log.Println("write:", err)
+				return nil
 			}
-		case "GUILD_MEMBER_LIST_UPDATE":
-			log.Println("Received GUILD_MEMBER_LIST_UPDATE event")
-			parsed := parseGuildMemberListUpdate(decoded)
-			if parsed["guild_id"].(string) == ds.GuildID && (containsType(parsed["types"].([]interface{}), "SYNC") || containsType(parsed["types"].([]interface{}), "UPDATE")) {
-				for elem, index := range parsed["types"].([]interface{}) {
-					if index == "SYNC" {
-						log.Println("Processing SYNC")
-						if len(parsed["updates"].([]interface{})[elem].([]interface{})) == 0 {
-							ds.EndScraping = true
-							log.Println("No updates in SYNC, ending scraping.")
-							break
-						}
-						for _, item := range parsed["updates"].([]interface{})[elem].([]interface{}) {
-							if item.(map[string]interface{})["member"] != nil {
-								mem := item.(map[string]interface{})["member"].(map[string]interface{})
-								obj := map[string]interface{}{
-									"tag": mem["user"].(map[string]interface{})["username"].(string) + "#" + mem["user"].(map[string]interface{})["discriminator"].(string),
-									"id":  mem["user"].(map[string]interface{})["id"].(string),
-								}
-								usersMutex.Lock()
-								users = append(users, obj)
-								usersMutex.Unlock()
-							}
-						}
-					} else if index == "UPDATE" {
-						log.Println("Processing UPDATE")
-						for _, item := range parsed["updates"].([]interface{})[elem].([]interface{}) {
-							if item.(map[string]interface{})["member"] != nil {
-								mem := item.(map[string]interface{})["member"].(map[string]interface{})
-								obj := map[string]interface{}{
-									"tag": mem["user"].(map[string]interface{})["username"].(string) + "#" + mem["user"].(map[string]interface{})["discriminator"].(string),
-									"id":  mem["user"].(map[string]interface{})["id"].(string),
-								}
-								usersMutex.Lock()
-								users = append(users, obj)
-								usersMutex.Unlock()
-							}
-						}
+		} else if response["t"].(string) == "READY_SUPPLEMENTAL" {
+			sendData := map[string]interface{}{
+				"op": 14,
+				"d": map[string]interface{}{
+					"guild_id":   server,
+					"typing":     true,
+					"activities": true,
+					"threads":    true,
+					"channels": map[string]interface{}{
+						channel: [][]int{{0, 99}, {100, 199}, {200, 299}},
+					},
+				},
+			}
+			if err := conn.WriteJSON(sendData); err != nil {
+				log.Println("write:", err)
+				return nil
+			}
+		} else if response["t"].(string) == "GUILD_MEMBER_LIST_UPDATE" {
+			items := response["d"].(map[string]interface{})["ops"].([]interface{})
+			for _, item := range items {
+				for _, member := range item.(map[string]interface{})["items"].([]interface{}) {
+					if member.(map[string]interface{})["member"] != nil {
+						users = append(users, member.(map[string]interface{})["member"].(map[string]interface{})["user"].(map[string]interface{})["id"].(string))
 					}
-					ds.LastRange++
-					ds.Ranges = getRanges(ds.LastRange, 100, int(ds.Guilds[ds.GuildID]["member_count"].(float64)))
-					time.Sleep(350 * time.Millisecond)
-					ds.scrapeUsers()
 				}
 			}
-			if ds.EndScraping {
-				log.Println("End scraping, closing connection.")
-				ds.Conn.Close()
-			}
+			return users
 		}
 	}
-}
-
-func scrape(token, guildID, channelID string) []map[string]interface{} {
-	ds := &DiscordSocket{
-		Token:         token,
-		GuildID:       guildID,
-		ChannelID:     channelID,
-		SocketHeaders: map[string]string{"Accept-Encoding": "gzip, deflate, br", "Accept-Language": "en-US,en;q=0.9", "Cache-Control": "no-cache", "Pragma": "no-cache", "Sec-WebSocket-Extensions": "permessage-deflate; client_max_window_bits", "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_6) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.1.2 Safari/605.1.15"},
-		Guilds:        make(map[string]map[string]interface{}),
-		Members:       make(map[string]interface{}),
-		Ranges:        [][]int{{0, 0}},
-		LastRange:     0,
-		PacketsRecv:   0,
-		Mutex:         sync.Mutex{},
-	}
-
-	done := make(chan struct{})
-	go func() {
-		defer close(done)
-		ds.run()
-	}()
-
-	<-done
-	return users
-}
-
-func containsRange(ranges [][]int, target []int) bool {
-	for _, r := range ranges {
-		if r[0] == target[0] && r[1] == target[1] {
-			return true
-		}
-	}
-	return false
-}
-
-func containsType(types []interface{}, target string) bool {
-	for _, t := range types {
-		if t.(string) == target {
-			return true
-		}
-	}
-	return false
-}
-
-func (ds *DiscordSocket) runForever() {
-	url := "wss://gateway.discord.gg/?encoding=json&v=9"
-	conn, _, err := websocket.DefaultDialer.Dial(url, http.Header{"Sec-WebSocket-Extensions": {"permessage-deflate; client_max_window_bits"}})
-	if err != nil {
-		log.Fatal("Error connecting to WebSocket:", err)
-	}
-	ds.Conn = conn
-
-	log.Println("WebSocket connection established.")
-
-	go ds.sockOpen()
-
-	done := make(chan struct{})
-	go func() {
-		defer close(done)
-		for {
-			_, message, err := conn.ReadMessage()
-			if err != nil {
-				log.Println("Error reading message:", err)
-				return
-			}
-			go ds.sockMessage(message)
-		}
-	}()
-
-	<-done
-}
-
-func (ds *DiscordSocket) send(message string) {
-	//ds.Mutex.Lock()
-	//defer ds.Mutex.Unlock()
-
-	err := ds.Conn.WriteMessage(websocket.TextMessage, []byte(message))
-	if err != nil {
-		log.Println("Error writing message:", err)
-		return
-	}
-	log.Println("Sent message:", message)
 }
